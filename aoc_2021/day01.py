e2e76d7b44918113edc82908cafc206df56ec8f4
Geoff Beier <geoff@tuxpup.com>
2021-12-01 00:57:40 -0500

    day 1 solution after some minor golf. not bothering with any more than that.

---
+++ aoc_2021/day01.py
@@ -0,0 +1,31 @@
+from typing import List
+import aocd
+from . import aoc_year
+
+aoc_day = 1
+
+
+def preprocess(data: List[str]):
+    measurements = [
+        int(item) for item in aocd.get_data(day=aoc_day, year=aoc_year).splitlines()
+    ]
+    return measurements
+
+
+def part1(measurements: List[int]):
+    return len(
+        [
+            measurement
+            for n, measurement in enumerate(measurements[1:])
+            if measurement > measurements[n]
+        ]
+    )
+
+
+def part2(measurements: List[int]):
+    return part1(
+        [
+            measurements[i] + measurements[i + 1] + measurements[i + 2]
+            for i in range(0, len(measurements) - 2)
+        ]
+    )
