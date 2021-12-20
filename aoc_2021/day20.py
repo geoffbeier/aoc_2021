c393cf523ae5e82f23f350f576e3e9f8ff22156b
Geoff Beier <geoff@tuxpup.com>
2021-12-20 12:11:40 -0500

    working numpy solution. much slower than string comparison version.

--- aoc_2021/day20.py
+++ aoc_2021/day20.py
@@ -6,6 +6,7 @@ from collections import defaultdict, namedtuple
 from dataclasses import dataclass
 from itertools import product
 from math import prod
+import numpy
 from typing import List, Dict, Any, Tuple
 import aocd
 from . import aoc_year
@@ -28,6 +29,8 @@ except AssertionError:
     else:
         logger.error(f"Unable to guess a day from {__name__}. Exiting")
         sys.exit(1)
+
+DEBUG = False
 
 # My first attempt was to use a set of points that were "on". This worked fine for the demo data, but fell down with the
 # real dataset. I suspect this is because the enhancer in the real input had 000000000 set to '#' instead of '.' and that
@@ -56,7 +59,7 @@ class TrenchImage:
         image_lines = image_lines.split()
         self.w = len(image_lines[0])
         self.h = len(image_lines)
-        self.grid = [[char_map[c] for c in line] for line in image_lines]
+        self.grid = numpy.array([[char_map[c] for c in line] for line in image_lines])
         self.enhancement_algorithm = [char_map[c] for c in enhancment_algorithm]
         self.filler = 0
         self.expand_border()
@@ -64,40 +67,34 @@ class TrenchImage:
     def expand_border(self, increment: int = 1):
         new_h = self.h + 2 * increment
         new_w = self.w + 2 * increment
-        new_grid = []
-        for row in self.grid:
-            row.insert(0, self.filler)
-            row.append(self.filler)
-            new_grid.append(row)
-        self.grid = new_grid
-        self.grid.insert(0, [self.filler for _ in range(new_w)])
-        self.grid.append([self.filler for _ in range(new_w)])
+        self.grid = numpy.insert(self.grid, [0, self.w], self.filler, axis=1)
+        self.grid = numpy.insert(
+            self.grid, [0, self.h], [self.filler for _ in range(new_w)], axis=0
+        )
         self.w = new_w
         self.h = new_h
 
     def enhance(self):
-        new_grid = []
+        new_grid = numpy.empty_like(self.grid)
 
         def index(xx, yy):
-            bits = ""
-            for dx, dy in pixel_region:
+            bits = 0
+            for bit, (dx, dy) in enumerate(pixel_region):
                 if 0 <= xx + dx < self.w and 0 <= yy + dy < self.h:
-                    bits += f"{self.grid[yy + dy][xx + dx]}"
+                    bits |= self.grid[yy + dy][xx + dx] << (8 - bit)
                 else:
-                    bits += f"{self.filler}"
-            return int(bits, 2)
+                    bits |= self.filler << (8 - bit)
+            return bits
 
         for y in range(self.h):
-            row = []
             for x in range(self.w):
-                row.append(self.enhancement_algorithm[index(x, y)])
-            new_grid.append(row)
+                new_grid[y][x] = self.enhancement_algorithm[index(x, y)]
         self.grid = new_grid
         self.filler = self.enhancement_algorithm[int(f"{self.filler}" * 9, 2)]
         self.expand_border()
 
     def to_bitmap(self, dark: str = ".", light: str = "#"):
-        bitmap = "\n".join(["".join(row) for row in self.grid])
+        bitmap = "\n".join(["".join([str(x) for x in row]) for row in self.grid])
         bitmap = bitmap.replace("1", light)
         bitmap = bitmap.replace("0", dark)
         return bitmap
@@ -117,16 +114,20 @@ def preprocess():
 
 def part1(context: AOCContext):
     steps = 2
+    if DEBUG:
+        logger.debug(f"before\n{context.image.to_bitmap()}")
     for _ in range(steps):
         context.image.enhance()
-    return str(sum([c.count(1) for c in context.image.grid]))
+        if DEBUG:
+            logger.debug(f"after {_}\n{context.image.to_bitmap()}")
+    return str(numpy.count_nonzero(context.image.grid == 1))
 
 
 def part2(context: AOCContext):
     steps = 50
     for i in range(steps):
         context.image.enhance()
-    return str(sum([c.count(1) for c in context.image.grid]))
+    return str(numpy.count_nonzero(context.image.grid == 1))
 
 
 tests = [
@@ -146,6 +147,8 @@ tests = [
 
 
 def test(start: int = 0, finish: int = len(tests)):
+    global DEBUG
+    DEBUG = True
     for i, t in enumerate(tests[start:finish]):
         aocd.get_data = lambda *_, **__: t[0]
         result = t[2](preprocess())
